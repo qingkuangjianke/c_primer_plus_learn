cd142214469d173d3f2869b3ecce38015f6bca75
qingkuangjianke <1034507693@qq.com>
2025-07-25 10:24:47 +0800

    add test

---
+++ 2/chapter2.12.01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+int main() {
+
+    printf("qingkuang jianke是个\n");
+    printf("qingkuang \njianke\n");
+    printf("qingkuang");
+    printf(" jianke\n");
+    system("pause");
+    int a=69;
+    printf("%c", a);
+    system("pause");
+    return 0;
+}
